0847a65e0acc9ce8b1fb34861419081827973680
金永康 <jink6688@aliyun.com>
2025-10-27 15:38:36 +0800

    feat-分出路由module

--- crates/api/src/route/mod.rs
+++ crates/api/src/route/mod.rs
@@ -13,24 +13,25 @@ pub use health as other_health;
 use crate::user;
 
 pub fn api_route(state: AppState) -> Router {
-    let none_auth_route = none_auth_route();
-    let auth_route = middleware::apply(auth_route(state));
-    Router::merge(none_auth_route, auth_route)
+    let auth_route_with_middleware = middleware::apply(auth_route(state));
+    Router::new()
+        .merge(none_auth_route())  // 先合并无需认证的路由
+        .merge(auth_route_with_middleware)       // 再合并需要认证的路由（带中间件）
+        .fallback(fallback_handler)
 }
 
 pub fn none_auth_route() -> Router {
     Router::new()
         .route("/health", get(other_health::health))
         .route("/example/token", get(other_health::example_user))
-        .route("/example/user", get(other_health::example_user_info))
 }
 
 pub fn auth_route(state: AppState) -> Router {
     Router::new()
         .route("/users", post(user::create_user).get(user::list_users).put(user::update_user))
         .route("/users/:id", get(user::get_user).delete(user::del_user))
+        .route("/example/user", get(other_health::example_user_info))
         .with_state(state)
-        .fallback(fallback_handler)
 }
 
 /// Fallback handler for unmatched routes
